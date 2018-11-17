17a9bb4e21035ee2bfccc31e57f31b7ba77b00c2
John Ahn <john@launchpartner.co>
2018-11-17 06:36:09 -0800

    updated catch-all.routes and unignored client/build

---
+++ client/build/precache-manifest.1769228e0c463d89a6d430f4bda45e98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed9677fa8bbba679c0b0",
+    "url": "/static/js/main.ed9677fa.chunk.js"
+  },
+  {
+    "revision": "55b6b212b98c490d0070",
+    "url": "/static/js/1.55b6b212.chunk.js"
+  },
+  {
+    "revision": "ed9677fa8bbba679c0b0",
+    "url": "/static/css/main.9106f59e.chunk.css"
+  },
+  {
+    "revision": "32059bdf07eaee827ed3aad1a9b7532c",
+    "url": "/index.html"
+  }
+];
