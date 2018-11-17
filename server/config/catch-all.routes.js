fd8f297a0063381c071e4a0dfa354a64039d7229
John Ahn <john@launchpartner.co>
2018-11-17 06:22:11 -0800

    updated catch-all.routes

--- server/config/catch-all.routes.js
+++ server/config/catch-all.routes.js
@@ -5,7 +5,7 @@ router.all('*', function(request, response) {
     // console.log('***** catch-all.routes *****');
     // console.log('***** route', request.url, ' not found; sending index.html ******');
     // console.log('*** hmm something went awry, my friend, because this is catch-all-routes ***');
-    response.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    response.sendFile(path.join(__dirname, 'client/build','index.html'));
 });
 
 module.exports = router;
